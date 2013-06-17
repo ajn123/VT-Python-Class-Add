3be3e2dada5fd9f4c80181ff429ba49adb5030ad
Jason Riddle <jr1285@vt.edu>
2013-06-16 22:46:47 -0400

    Modified login.feature and login.py
    Added 'wip' and 'login' tags to the login scenarios in login.feature
    Imported functions from the main_program module in login.py

--- features/login.feature
+++ features/login.feature
@@ -1,7 +1,9 @@
 Feature: Logging in
 
+    @login @wip
     Scenario: Wrong Username and Password
         Given I enter my username as "fake_user" and my password as "fake_password"
 
+    @login @wip
     Scenario: Wrong Username and Password
         Given I enter my username as "pid" and my password as "password"
--- features/steps/login.py
+++ features/steps/login.py
@@ -1,7 +1,6 @@
 from behave import *
+from main_program import *
 
 @given('I enter my username as "{username}" and my password as "{password}"')
 def impl(context, username, password):
-    import main_program
-    x = raw_input('co   ')
-    assert main_program.login(username, password) is False
+    assert login(username, password) is False
